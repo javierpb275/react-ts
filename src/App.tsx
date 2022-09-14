eed5550b56e4c5f9a615ed76fb8e3213ab86376c
javierpb275 <javierpb275@gmail.com>
2022-09-14 13:51:34 +0200

    managing state with useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("make your bed"), new Todo("brush your teeth")];
-  const addTodoHandler = (text: string) => {}
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
   return (
     <div>
-      <NewTodo onAddTodo={addTodoHandler}/>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
