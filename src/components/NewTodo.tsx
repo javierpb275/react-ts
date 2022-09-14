419ff9e8c9a79f15e96a89568ebfb873b7f91560
javierpb275 <javierpb275@gmail.com>
2022-09-14 13:23:06 +0200

    NewTodo form component created

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NewTodo: React.FC = () => {
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
