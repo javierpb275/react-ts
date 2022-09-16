d5b71bb3b82e23ad8331f5af35ef8a5e454888d1
javierpb275 <javierpb275@gmail.com>
2022-09-16 17:01:48 +0200

    redux toolkit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,40 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../store";
+import {
+  increment,
+  decrement,
+  changeIncrementAmount,
+} from "../store/counterReducer";
 
 export interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+  const incrementAmount = useSelector(
+    (state: RootState) => state.counter.incrementAmount
+  );
+  function handleChange(incrementAmountValue: string) {
+    dispatch(changeIncrementAmount(Number(incrementAmountValue)));
+  }
+
   return (
     <div>
       <p>HOME PAGE</p>
-      <Link to="/about">Go to the about page</Link><br/>
-      <button onClick={() => navigate('/layout/3')}>Go to layout, with a number</button>
+      <Link to="/about">Go to the about page</Link>
+      <br />
+      <button onClick={() => navigate("/layout/3")}>
+        Go to layout, with a number
+      </button>
+      <h2>COUNTER:</h2>
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <button onClick={() => dispatch(decrement())}>-1</button>
+      <input type="number" onChange={(e) => handleChange(e.target.value)} />
+      <p>count: {count}</p>
+      <p>increment amount: {incrementAmount}</p>
     </div>
   );
 };
